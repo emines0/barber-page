4f7cc914634986c3ef0254dfff3971faf012f546
emines0 <emin.sefa1@gmail.com>
2023-10-10 09:47:21 +0200

    media querries tunning

--- js/script.js
+++ js/script.js
@@ -1,95 +1,81 @@
-const navbar = document.getElementById("main-nav");
-const menuBtn = document.querySelector('.fa-bars');
-const closeBtn = document.querySelector('.fa-close');
-const navigation = document.querySelector('.nav-container');
-const navReservation = document.querySelector('.nav-reservation');
-const navLinks = document.querySelectorAll('.link');
-
+const navbar = document.getElementById('main-nav')
+const menuBtn = document.querySelector('.fa-bars')
+const closeBtn = document.querySelector('.fa-close')
+const navigation = document.querySelector('.nav-container')
+const navReservation = document.querySelector('.nav-reservation')
+const navLinks = document.querySelectorAll('.link')
 
 /* Header on scroll */
-window.addEventListener("scroll", () => {
-	if(innerWidth >= 960) {
-	if (scrollY >= 150) {
-		navbar.classList.add("top");
-	} else {
-		navbar.classList.remove("top");
-	}
-}
-});
+window.addEventListener('scroll', () => {
+    if (innerWidth >= 1170) {
+        if (scrollY >= 150) {
+            navbar.classList.add('top')
+        } else {
+            navbar.classList.remove('top')
+        }
+    }
+})
 
 /* Scrolling 150px above section*/
-$("#main-nav a, .btn").on("click", function (e) {
-	if (this.hash !== "") {
-		e.preventDefault();
+$('#main-nav a, .btn').on('click', function (e) {
+    if (this.hash !== '') {
+        e.preventDefault()
 
-		const hash = this.hash;
+        const hash = this.hash
 
-		$("html, body").animate(
-			{
-				scrollTop: $(hash).offset().top - 200,
-				
-			},
-			50
-		);
-		
-	}
-});
-
-
-
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top - 200,
+            },
+            50
+        )
+    }
+})
 
 function linkClick(e) {
-		navLinks.forEach((link) => {
-		link.addEventListener('click', () => {
-			navigation.style.display = 'none';
-			navReservation.style.display = 'none';
-			menuBtn.style.display = 'block';
-			closeBtn.style.display = 'none';
-		})
-	})
+    navLinks.forEach((link) => {
+        link.addEventListener('click', () => {
+            navigation.classList.add = 'd-none'
+            navReservation.style.display = 'none'
+            menuBtn.style.display = 'block'
+            closeBtn.style.display = 'none'
+        })
+    })
 }
 
 window.addEventListener('resize', () => {
-
-	if(innerWidth > 960) {
-	navigation.style.display = 'grid';
-	navReservation.style.display = 'flex';
-	menuBtn.style.display = 'none';
-	closeBtn.style.display = 'none';
-	}else {
-	navigation.style.display = 'none';
-	navReservation.style.display = 'none';
-	menuBtn.style.display = 'grid';
-	closeBtn.style.display = 'none';
-	}
-
-
+    if (innerWidth > 1170) {
+        navigation.style.display = 'grid'
+        navReservation.style.display = 'flex'
+        menuBtn.style.display = 'none'
+        closeBtn.style.display = 'none'
+    } else {
+        navigation.style.display = 'none'
+        navReservation.style.display = 'none'
+        menuBtn.style.display = 'grid'
+        closeBtn.style.display = 'none'
+    }
 })
-
-
 
 // Mobile version
 
-if(innerWidth < 960) {
-	// Event Listeners
-menuBtn.addEventListener('click', () => {
+if (innerWidth < 1170) {
+    // Event Listeners
+    menuBtn.addEventListener('click', () => {
+        navigation.style.display = 'grid'
+        navReservation.style.display = 'flex'
+        menuBtn.style.display = 'none'
+        closeBtn.style.display = 'inline-block'
+    })
 
-	navigation.style.display = 'grid';
-	navReservation.style.display = 'flex';
-	menuBtn.style.display = 'none';
-	closeBtn.style.display = 'inline-block';
-})
+    closeBtn.addEventListener('click', () => {
+        navigation.style.display = 'none'
+        navReservation.style.display = 'none'
+        menuBtn.style.display = 'inline-block'
+        closeBtn.style.display = 'none'
+    })
 
-closeBtn.addEventListener('click', () => {
-
-	navigation.style.display = 'none';
-	navReservation.style.display = 'none';
-	menuBtn.style.display = 'inline-block';
-	closeBtn.style.display = 'none';
-})
-
-	//Global Scope
-	linkClick();
-} else  {
-
+    //Global Scope
+    linkClick()
+} else {
 }
